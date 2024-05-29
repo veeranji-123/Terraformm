b03a8e6e97f9f0e18390e82a70a719da5dbf9fcb
1s2r3i4n5u <169056492+1s2r3i4n5u@users.noreply.github.com>
2024-05-29 12:13:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_instance" "wordpress_instance" {
   ami                         = "ami-0d94353f7bad10668"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "143143"
+  key_name                    = "789365"
   vpc_security_group_ids      = ["${aws_security_group.python_sg.id}"]
   subnet_id                   = aws_subnet.main.id
   associate_public_ip_address = true
