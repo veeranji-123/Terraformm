364c1de740fb7444ec82ece2871bb49bb303b902
1s2r3i4n5u <169056492+1s2r3i4n5u@users.noreply.github.com>
2024-05-29 12:29:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ variable "subnet_cidr" {
 
 # Creating EC2 instance
 resource "aws_instance" "wordpress_instance" {
-  ami                         = "ami-0d94353f7bad10668"
+  ami                         = "ami-06efb824bfe850db6"
   instance_type               = "t2.micro"
   count                       = 1
   key_name                    = "143143"
